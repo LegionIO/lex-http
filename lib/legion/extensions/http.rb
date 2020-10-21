0e3ad624881e27f273f3989b3ab426d17c323eca
Esity <matthewdiverson@gmail.com>
2020-10-21 14:00:49 -0500

    Updating some base things

--- lib/legion/extensions/http.rb
+++ lib/legion/extensions/http.rb
@@ -1,10 +1,9 @@
 require 'legion/extensions/http/version'
-require 'legion/extensions'
 
 module Legion
   module Extensions
     module Http
-      extend Legion::Extensions::Core
+      extend Legion::Extensions::Core if Legion::Extensions.const_defined? :Core
 
       def self.default_settings
         {
